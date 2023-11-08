9f22c72f5376afbef3b9bdb86944dec0818a2763
Gabriel Mioni <gabrielmionibastos@gmail.com>
2023-11-08 20:05:22 -0300

    Empréstimo
    + Correções feitas no arquivo HTML.
    + Criação de botão "Realizar Empréstimo" que aparece a função do botão "Calcular" ser realizada.

--- pfisica/emprestimo/js/app.js
+++ pfisica/emprestimo/js/app.js
@@ -1,11 +1,14 @@
 $('#formulario').submit(function(e) {
     e.preventDefault();
 
-        const valorEmprestimo = parseFloat($('#valor-emp').val());
-         const prazoMeses = parseInt($('#prazo-emp').val());
-        const taxaJurosMensal = 0.065;
+    const valorEmprestimo = parseFloat($('#valor-emp').val());
+    const prazoMeses = parseInt($('#prazo-emp').val());
+    const taxaJurosMensal = 0.065;
 
-         const parcelaMensal = (valorEmprestimo * taxaJurosMensal) / (1 - Math.pow(1 + taxaJurosMensal, -prazoMeses));
-            
-        $('#pagamento').text(`Sua parcela mensal será de R$${parcelaMensal.toFixed(2)}`);
-}); 
+    const parcelaMensal = (valorEmprestimo * taxaJurosMensal) / (1 - Math.pow(1 + taxaJurosMensal, -prazoMeses));
+
+    $('#pagamento').text(`Sua parcela mensal será de R$${parcelaMensal.toFixed(2)}`);
+
+    document.getElementById('realizar-emprestimo').classList.remove('d-none');
+    document.getElementById('realizar-emprestimo').classList.add('d-grid');
+});
